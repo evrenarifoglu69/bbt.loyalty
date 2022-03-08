1eb86e49fce8ad6dc686333a8c86741051428bb8
fatih-ozen-safrantek <fatih.ozen@safrantek.com>
2022-03-08 09:29:20 +0300

    ui changes

--- src/Ui/src/app/screens/campaign-definition-container/campaign-definition-list/campaign-definition-list.component.ts
+++ src/Ui/src/app/screens/campaign-definition-container/campaign-definition-list/campaign-definition-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IAngularMyDpOptions} from "angular-mydatepicker";
 
 @Component({
   selector: 'app-campaign-definition-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./campaign-definition-list.component.scss']
 })
 export class CampaignDefinitionListComponent implements OnInit {
+
+  dpOptions: IAngularMyDpOptions = {
+    dateRange: false,
+    dateFormat: 'dd/mm/yyyy',
+  };
+  locale: string = 'tr';
 
   constructor() { }
 
